e83bd9fbd0221794e4e56dcb6ffed7e05377a146
Nicolas Arcelin <nicolasarcelin@Nicolass-MacBook-Pro.local>
2024-03-15 09:04:22 +0100

    deployed now connecting supabase

---
+++ app/lib/initSupabase.tsx
@@ -0,0 +1,13 @@
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
