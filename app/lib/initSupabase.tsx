69d993a8518c69a92aa45bbd82d1061f8b6f8206
Nicolas Arcelin <nicolasarcelin@Nicolass-MacBook-Pro.local>
2024-04-18 20:26:28 +0200

    Environmental Keys added to Vercel

--- app/lib/initSupabase.tsx
+++ app/lib/initSupabase.tsx
@@ -7,9 +7,15 @@
 
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl: string = process.env.SUPABASE_URL || '';
+const supabaseAnonKey: string = process.env.SUPABASE_ANON_KEY || '';
 
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+// const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+// const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+export const supabase: SupabaseClient = createClient(
+  supabaseUrl,
+  supabaseAnonKey,
+);
 
 // export const supabase = createClient(supabaseUrl, supabaseAnonKey);
